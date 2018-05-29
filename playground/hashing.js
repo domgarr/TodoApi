fe120248c81dbb03541bd3610e738b99b9344327
Domenic Garreffa <garreffd@uwindsor.ca>
2018-05-29 16:49:02 -0400

    add password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
 
+const bcrypt = require('bcryptjs');
+
+var password = '123123';
+
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
+
+var hashedPassword = 'hashedpasswordvaluejfkldjafklas;l';
+
+bcrypt.compare(password, hashPassword, (err, res)  => {
+    console.log(res);
+});
+
+/*
 var data = {
     data: 10
 };
 
 var token = jwt.sign(data, '123data');
 
-
-
-
-/*
 var message = 'I am a user number 3';
 var hash = SHA256(message).toString();
 

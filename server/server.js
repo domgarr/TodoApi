9e67f1387e10883f31e98a29799ffe6787c27d32
Domenic Garreffa <garreffd@uwindsor.ca>
2018-05-29 23:07:49 -0400

    add POST user/me/login and DELETE user/me/token

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -76,6 +77,50 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.status(200).header('x-auth', token).send(user);
+        })
+        
+    }).catch((e) => {
+        res.status(401).send();
+    });
+
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+});
+
+    /*  How i did it.
+    if(body.email != null && body.password != null) {
+        User.findOne({email: body.email}).then((user) =>{
+            if(!user){
+                res.status(401).send({});
+                return;
+            }
+
+                   bcrypt.compare(body.password, user.password, (err, _res) => {
+                    console.log(_res);   
+                    if(_res){
+                            user.generateAuthToken().then((token) => {
+                            res.status(200).header('x-auth', token).send({email: user.email});
+                            });
+                        }
+                   });
+        });
+    }else{
+        res.status(401).send({});
+    }
+
+    */
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,28 +4,12 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const id = new ObjectID(123);
+const id = todos[2]._id;
 
-const todos = [{
-    text: "Apply to one job"
-},
-{
-    text: "Apply to one more job"
-},
-,
-{
-    _id: id ,
-    text: "Apply to one or more jobs"
-}
-];
-
-beforeEach((done) =>{
-    Todo.remove({}).then(() => {
-        //Returning here allows to chain callbacks.
-    return Todo.insertMany(todos);
-}).then(() => done());
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () =>{
     it('should create new todo', done =>{
@@ -106,3 +90,73 @@ describe('POST /todos', () =>{
         })
     });
     
+    describe('GET /users/me', () => {
+        it('should return user if authenticated', (done) => {
+            request(app)
+                .get('/users/me')
+                .set("x-auth", users[0].tokens[0].token )
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body._id).toBe(users[0]._id.toHexString());
+                    expect(res.body.email).toBe(users[0].email);
+                })
+                .end(done);
+        });
+
+        it('should return 401 if not authenticated', (done) => {
+            request(app)
+                .get('/users/me')
+                .expect(401)
+                .expect((res) => {
+                    expect(Object.keys(res.body).length).toBe(0);
+                    expect(res.body).toEqual({});
+                })
+                .end(done);
+        });
+    });
+
+    describe('POST /users', () => {
+        it('should create a user', (done) => {
+            const validEmail = 'garreffd@uwindsor.ca';
+            const validPassword = 'hello123';
+
+            request(app)
+                .post('/users')
+                .send({
+                    email: validEmail,
+                    password: validPassword
+                })
+                .expect(200)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).toExist();
+                    expect(res.body._id).toExist();
+                    expect(res.body.email).toBe(validEmail);
+                })
+                .end(done);
+        });
+
+        it('should return validation error if request invalid', (done) => {
+
+        });
+
+        it('should not create user if email is in use', (done) => {
+
+        });
+    });
+
+    describe('POST /users/login', () => {
+        if('should login an existing user', (done) => {
+            const email = 'garreffd@uwindsor.ca';
+            const password = 'hello123';
+
+            request(app)
+                .post('/users/login')
+                .send({email, password})
+                .expect(200)
+                .expect((res) => {
+                    expect(res.headers['x-auth']).toExist();
+                    expect(res.body.email).toBe(email);
+                })
+                .end(done);
+        });
+    });
